4302d7fc4eadcc17bd476e7d24c6a0863ba5bc22
prashanna <prashannadhami14@gmail.com>
2023-12-31 17:18:36 +0545

    push

--- js/index.js
+++ js/index.js
@@ -80,3 +80,21 @@ function clearErrorsAndBorders() {
     element.style.border = "1px solid #ccc"; // Reset border to the default color
   });
 }
+// video play on hover
+
+        // Targeting video element  
+        let clip = document.querySelector(".vid") 
+  
+        /* Applying mouseover event on video clip  
+        and then we call play() function to play  
+        the video when the mouse is over the video */ 
+        clip.addEventListener("mouseover", function (e) { 
+            clip.play(); 
+        }) 
+  
+        /* Applying mouseout event on video clip  
+        and then we call pause() function to stop  
+        the video when the mouse is out the video */ 
+        clip.addEventListener("mouseout", function (e) { 
+            clip.pause(); 
+        }) 
